8239bd09a9443076e941db494198a18f338571f0
blackwine93v <daoquocvuong@gmail.com>
2022-10-29 23:33:26 +0700

    chore: backend - add swagger document

--- apps/backend/src/employee/dto/create-employee.dto.ts
+++ apps/backend/src/employee/dto/create-employee.dto.ts
@@ -1,25 +1,37 @@
-import { IsEmail, IsPhoneNumber, IsIn, IsUrl, MinLength, MaxLength, IsAlpha } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsPhoneNumber, IsUrl, MinLength, MaxLength, IsAlpha, IsEnum } from 'class-validator';
+
+enum Gender {
+  M = 'M',
+  F = 'F'
+}
 
 export class CreateEmployeeDto {
   @IsAlpha()
   @MinLength(6)
   @MaxLength(10)
+  @ApiProperty({ minLength: 6, maxLength: 10 })
   first_name: string;
 
   @IsAlpha()
   @MinLength(6)
   @MaxLength(10)
+  @ApiProperty({ minLength: 6, maxLength: 10 })
   last_name: string;
 
   @IsEmail()
+  @ApiProperty()
   email: string;
 
+  @ApiProperty()
   @IsPhoneNumber('LK')
   number: string;
 
-  @IsIn(['M', 'F'])
-  gender: string;
+  @ApiProperty({ enum: Gender })
+  @IsEnum(Gender)
+  gender: Gender;
 
+  @ApiProperty()
   @IsUrl()
   photo: string;
 }
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,6 +4,7 @@
  */
 
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
@@ -13,6 +14,15 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Employee management API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
