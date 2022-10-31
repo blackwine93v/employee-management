def4974c7de6b5d4999283996dfeba8ff2a4fa42
blackwine93v <daoquocvuong@gmail.com>
2022-10-31 14:51:58 +0700

    chore: frontend - redirect "/" to "employee/list"

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -15,6 +15,11 @@ const nextConfig = {
   redirects: async () => {
     return [
       {
+        source: '/',
+        destination: '/employee/list',
+        permanent: true,
+      },
+      {
         source: '/employee',
         destination: '/employee/list',
         permanent: true,
