096667e63ae4b193ed8e212319fed3cbef3c47bf
blackwine93v <daoquocvuong@gmail.com>
2022-10-31 14:53:05 +0700

    feat: frontend - setup test

---
+++ apps/frontend/helpers/testUtils.tsx
@@ -0,0 +1,47 @@
+import React, { PropsWithChildren } from 'react';
+import { render } from '@testing-library/react';
+import type { RenderOptions } from '@testing-library/react';
+import { configureStore } from '@reduxjs/toolkit';
+import type { PreloadedState } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+import type { AppStore, RootState } from 'apps/frontend/redux/store';
+// As a basic setup, import your same slice reducers
+import employeeReducer from 'apps/frontend/pages/employee/reduxSlice';
+import ViewModeProvider from '../context/ViewModeContext';
+import { SnackbarProvider } from 'notistack';
+
+// This type interface extends the default options for render from RTL, as well
+// as allows the user to specify other things such as initialState, store.
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: PreloadedState<RootState>;
+  store?: AppStore;
+}
+
+export function renderWithProviders(
+  ui: React.ReactElement,
+  {
+    preloadedState = {},
+    // Automatically create a store instance if no store was passed in
+    store = configureStore({
+      reducer: { employee: employeeReducer },
+      preloadedState,
+    }),
+    ...renderOptions
+  }: ExtendedRenderOptions = {}
+) {
+  function Wrapper({ children }: PropsWithChildren<any>): JSX.Element {
+    return (
+      <Provider store={store}>
+        <ViewModeProvider>
+          <SnackbarProvider maxSnack={3}>
+            {children}
+          </SnackbarProvider>
+        </ViewModeProvider>
+      </Provider>
+    );
+  }
+
+  // Return an object with the store and all of RTL's query functions
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+}
--- apps/frontend/redux/store.ts
+++ apps/frontend/redux/store.ts
@@ -1,15 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {
+  combineReducers,
+  configureStore,
+  PreloadedState,
+} from '@reduxjs/toolkit';
 import employeeReducer from '../pages/employee/reduxSlice';
 
-const store = configureStore({
-  reducer: {
-    employee: employeeReducer,
-  },
+const rootReducer = combineReducers({
+  employee: employeeReducer,
 });
 
+export function setupStore(preloadedState?: PreloadedState<RootState>) {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+}
+
+const store = setupStore();
+
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
+
+export type AppStore = ReturnType<typeof setupStore>;
+
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
+export type AppDispatch = AppStore['dispatch'];
 
 export default store;
