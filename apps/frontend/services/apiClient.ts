3341646ee2c03406dac2a2ba3c7e6b107d85d000
blackwine93v <daoquocvuong@gmail.com>
2022-11-10 16:40:40 +0700

    chore: update backend API

--- apps/frontend/services/apiClient.ts
+++ apps/frontend/services/apiClient.ts
@@ -1,5 +1,5 @@
 import { apiClient as ApiClient } from '../openapi/apiClient';
 
 export const apiClient = new ApiClient({
-  BASE: `http://104.154.93.62:3333` // TODO move to env
+  BASE: `http://104.155.128.186:3333` // TODO move to env
 });
